961fe77621e573f10c4fde433793e8703fe4813d
Erik Thompson <C16erik.thompson@usafa.edu>
2014-01-28 11:04:35 -0700

    changes

--- Lab1_Thompson.vhd
+++ Lab1_Thompson.vhd
@@ -22,7 +22,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx primitives in this code.
@@ -30,30 +30,43 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity Lab1_Thompson is
-    Port ( A : in  STD_LOGIC;
-           B : in  STD_LOGIC;
-           C : in  STD_LOGIC;
-           X : out  STD_LOGIC;
-           Y : out  STD_LOGIC;
-           Z : out  STD_LOGIC);
+
+           --B : in  STD_LOGIC; [used for 3 bit]
+           --C : in  STD_LOGIC;
+           --X : out  STD_LOGIC;
+           --Y : out  STD_LOGIC;
+           --Z : out  STD_LOGIC
+	Port (  A : in  STD_LOGIC_VECTOR (7 downto 0);
+			  U : out STD_LOGIC_VECTOR (7 downto 0));
 end Lab1_Thompson;
 
 architecture Behavioral of Lab1_Thompson is
 
-signal C_NOT, B_NOT, D, E : STD_LOGIC;
+--signal A_NOT, B_NOT, C_NOT, D, E, F, G, H : STD_LOGIC;  [used for 3 bit]
 
-signal name1 : std_logic;
 
-signal name2: std_logic_vector(3 downto 0);
+signal name1 : std_logic_vector(7 downto 0); --input signal [vector]
+
+signal name2: std_logic_vector(7 downto 0); -- output signal [vector]
 
 begin
 
-C_NOT <= not C;
-B_NOT <= not B;
-D <= B_NOT and C;
-E <= B and C_NOT;
-X <= A or C or B;
-Y <= D or E;
-Z <= C;
+
+--A_NOT <= not A;
+--B_NOT <= not B;
+--C_NOT <= not C;
+--D <= A AND B_NOT AND C_NOT;  [used for 3 bit]
+--E <= A_NOT AND C;
+--F <= A_NOT AND B;
+--G <= C_NOT AND B;
+--H <= C AND B_NOT;
+--X <= D or E or F;
+--Y <= G or H;
+--Z <= C;
+
+name1 <= A;  --declaring the input signal 
+name2 <= STD_LOGIC_VECTOR (SIGNED(not A)+1); --performing the two's complement
+U <= name2; --defining the output signal for U 
+
 end Behavioral;
 
